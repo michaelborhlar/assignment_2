9d09bf9e28a32113ff07ab44a70d68162f6d8198
michaelborhlar <michealadefehinti09@gmail.com>
2025-10-22 23:13:58 +0100

    Add ListStringView and update URL patterns

--- string_analyzer/urls.py
+++ string_analyzer/urls.py
@@ -5,13 +5,16 @@ from .views import (
     GetAllStringsView,
     DeleteStringView,
     NaturalLanguageFilterView,
+    ListStringView,
 )
 
 urlpatterns = [
     path('strings', CreateStringView.as_view()),
+    path('strings', ListStringView.as_view(), name='list-strings'),
     path('strings/<str:string_value>', GetStringView.as_view()),
-    path('strings/filter-by-natural-language', NaturalLanguageFilterView.as_view()),
     path('strings/', GetAllStringsView.as_view()),  # trailing slash version
     path('strings/<str:string_value>/', DeleteStringView.as_view()),
+    path('strings/filter-by-natural-language', NaturalLanguageFilterView.as_view(), name='nl-filter'),
+    
 ]
 
